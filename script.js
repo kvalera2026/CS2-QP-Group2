89804060282409d6b9210a2a0fb7cb4e29f24d1e
naquino2026 <105036325+naquino2026@users.noreply.github.com>
2023-01-23 01:57:27 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor:'true',
+    pagination:{
+        el:".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true,
+    },
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints:{
+        0:{
+            slidesPerView:1,
+        },
+        520: {
+            slidesPerView:2,
+        },
+        950:{
+            slidesPerView:3,
+        }
+    }
+  });
+  document.querySelectorAll('#nav-tab>[data-bs-toggle="tab"]').forEach(el => {
+    el.addEventListener('shown.bs.tab', () => {
+      const target = el.getAttribute('data-bs-target')
+      const scrollElem = document.querySelector(`${target} [data-bs-spy="scroll"]`)
+      bootstrap.ScrollSpy.getOrCreateInstance(scrollElem).refresh()
+    })
+  })
+
+
+ 
+
